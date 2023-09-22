b3f32916823ee2680234531670f381f52e265a44
abdul mohsin <mohsin050595@gmail.com>
2023-09-22 21:11:27 +0530

    features added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 function Product({ product }) {
+  const dispatch = useDispatch();
   return (
-    <Link className="product_card" to={`products/${product.id}`}>
-      <Card>
+    <Card className="product_card">
+      <Link className="product_card_link" to={`products/${product.id}`}>
         <img
           style={{
             height: "150px",
             width: "150px",
-            padding: "2px",
-            alignSelf: "center",
+            padding: "4px",
           }}
           src={product?.image}
           alt={product?.title}
           loading="lazy"
         />
-        <Card.Body>
+        <Card.Body className="text-left">
           <p className="fs-5 fs-bold">{product?.title?.slice(0, 18)}</p>
           <p className=" fs-semibold">Price - {product?.price} $</p>
         </Card.Body>
-        <Button>Buy</Button>
-      </Card>
-    </Link>
+      </Link>
+      <Button onClick={() => dispatch(addToCart(product))}>Add to Cart</Button>
+    </Card>
   );
 }
 
