1449ce6a172666f6c7adcca2b1e01eb8f8a00a76
abdul mohsin <mohsin050595@gmail.com>
2023-09-22 21:07:50 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./components/product/Products";
 import ProductDetail from "./components/product/ProductDetail";
+import Navbar from "./components/common/Navbar";
+import Home from "./pages/Home";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Products />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
